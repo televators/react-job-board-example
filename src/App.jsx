458c160dcc6789373085dc0e68977394c93ac7c0
Justin Higgs <justinphiggs@gmail.com>
2024-05-26 15:29:58 -0400

    Added AddJob page, new controlled job form, and it POSTs to json server.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,33 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import AddJobPage from './pages/AddJobPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>,
-  ),
-);
-
 const App = () => {
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newJob),
+    });
+
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>,
+    ),
+  );
+
   return <RouterProvider router={router} />;
 };
 
