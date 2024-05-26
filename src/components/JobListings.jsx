4579ad537ca6a366d1026196457cb8c419f30c69
Justin Higgs <justinphiggs@gmail.com>
2024-05-26 13:45:16 -0400

    Added single Job page and route, along with single job loader.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = ( { isHome = false } ) => {
 
   useEffect( () => {
     const fetchJobs = async () => {
-      const apiURL = isHome ? 'http://localhost:5000/jobs?_limit=3' : 'http://localhost:5000/jobs';
+      const apiURL = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
 
       try {
         const res = await fetch( apiURL );
